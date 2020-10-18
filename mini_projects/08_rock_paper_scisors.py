4c89eb52f8c2cb446fbdc481fbdfd5483909cc82
Sergey Yurich <yurichsergey@gmail.com>
2020-10-18 16:33:02 +0300

    feat: Added random equals version of rock-paper-scissors

--- mini_projects/08_rock_paper_scisors.py
+++ mini_projects/08_rock_paper_scisors.py
@@ -1,3 +1,5 @@
+import random
+
 PAPER = 'paper'
 ROCK = 'rock'
 SCISSORS = 'scissors'
@@ -9,5 +11,11 @@ won_steps = {
 }
 
 player_choose = input()
+computer_choose = random.choice(list(won_steps))
 
-print('Sorry, but the computer chose ' + won_steps[player_choose])
+if player_choose == computer_choose:
+    print(f'There is a draw ({player_choose})')
+elif won_steps[player_choose] != computer_choose:
+    print(f'Well done. The computer chose {computer_choose} and failed')
+else:
+    print(f'Sorry, but the computer chose {computer_choose}')
