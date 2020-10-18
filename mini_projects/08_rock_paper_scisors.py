75667feac9b6829b467a25918e1511cf3ae85e6f
Sergey Yurich <yurichsergey@gmail.com>
2020-10-18 16:41:19 +0300

    refactor: Refactoring rock-paper-scisssors game

--- mini_projects/08_rock_paper_scisors.py
+++ mini_projects/08_rock_paper_scisors.py
@@ -1,21 +1,32 @@
 import random
 
-PAPER = 'paper'
-ROCK = 'rock'
-SCISSORS = 'scissors'
 
-won_steps = {
-    PAPER: SCISSORS,
-    ROCK: PAPER,
-    SCISSORS: ROCK
-}
+class RockPaperScissorsEngine:
+    PAPER = 'paper'
+    ROCK = 'rock'
+    SCISSORS = 'scissors'
 
-player_choose = input()
-computer_choose = random.choice(list(won_steps))
+    won_steps = {
+        PAPER: SCISSORS,
+        ROCK: PAPER,
+        SCISSORS: ROCK
+    }
 
-if player_choose == computer_choose:
-    print(f'There is a draw ({player_choose})')
-elif won_steps[player_choose] != computer_choose:
-    print(f'Well done. The computer chose {computer_choose} and failed')
-else:
-    print(f'Sorry, but the computer chose {computer_choose}')
+    def engine(self, player_choose: str) -> str:
+        computer_choose = random.choice(list(self.won_steps))
+        if player_choose == computer_choose:
+            answer = f'There is a draw ({player_choose})'
+        elif self.won_steps[player_choose] != computer_choose:
+            answer = f'Well done. The computer chose {computer_choose} and failed'
+        else:
+            answer = f'Sorry, but the computer chose {computer_choose}'
+        return answer
+
+
+def main():
+    player_choose = input()
+    answer = RockPaperScissorsEngine().engine(player_choose)
+    print(answer)
+
+
+main()
