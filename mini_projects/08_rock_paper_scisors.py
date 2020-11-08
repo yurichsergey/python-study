38f59c9eaa93f7ecd310b03ae4af1ba5f42c383a
Sergey Yurich <yurichsergey@gmail.com>
2020-11-08 05:39:10 +0300

    refactor: Refactor output object from rock-paper-engine

--- mini_projects/08_rock_paper_scisors.py
+++ mini_projects/08_rock_paper_scisors.py
@@ -1,6 +1,19 @@
 import random
 
 
+class AnswerStep:
+
+    def __init__(self, player_score: int, result_str: str):
+        self.player_score = player_score
+        self.result_str = result_str
+
+    def get_player_score(self) -> int:
+        return self.player_score
+
+    def get_result_str(self) -> str:
+        return self.result_str
+
+
 class RockPaperScissorsEngine:
     PAPER = 'paper'
     ROCK = 'rock'
@@ -17,16 +30,16 @@ class RockPaperScissorsEngine:
     def is_valid_choice(self, choice: str) -> bool:
         return choice in self.valid_steps
 
-    def action(self, player_choice: str) -> str:
+    def action(self, player_choice: str) -> AnswerStep:
         if not self.is_valid_choice(player_choice):
-            return 'Invalid input'
+            return AnswerStep(0, 'Invalid input')
         computer_choice = random.choice(self.valid_steps)
         if player_choice == computer_choice:
-            answer = f'There is a draw ({player_choice})'
+            answer = AnswerStep(50, f'There is a draw ({player_choice})')
         elif self.won_steps[player_choice] != computer_choice:
-            answer = f'Well done. The computer chose {computer_choice} and failed'
+            answer = AnswerStep(100, f'Well done. The computer chose {computer_choice} and failed')
         else:
-            answer = f'Sorry, but the computer chose {computer_choice}'
+            answer = AnswerStep(0, f'Sorry, but the computer chose {computer_choice}')
         return answer
 
 
@@ -37,11 +50,14 @@ def main():
         if player_choice == '!exit':
             print('Bye!')
             break
+        if player_choice == '!rating':
+            print('Bye!')
+            continue
         if not engine.is_valid_choice(player_choice):
             print('Invalid input')
             continue
         answer = engine.action(player_choice)
-        print(answer)
+        print(answer.get_result_str())
 
 
 main()
